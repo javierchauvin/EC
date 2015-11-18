12213bb7fe188584a7e33d9bfc32a9e43d3c036e
Derek Liu <htliu1992@gmail.com>
2015-11-17 23:03:22 -0500

    Derek Commit on Flash and Intro
    MSG:
    Modified cpp and hpp in correct format

--- Flash.cpp
+++ Flash.cpp
@@ -1,10 +1,133 @@
 #include "Flash.hpp"
-int main(){
-    Flash test;
-    test.Set(5, 10);
-    test.Display();
-    return 0;
+Flash::Flash(){
+    gameCharge = 0;
+    money = 0;
+    GenerateCard();
+}
+Flash::Flash(int money_from, int gameCharge_from){
+    money = money_from;
+    gameCharge = gameCharge_from;
+    GenerateCard();
+}
+Flash::~Flash(){
+    gameCharge = 0;
+    money = 0;
+    for(int i=0; i<144; ++i){
+        card[i].a = 0;
+        card[i].b = 0;
+        text[i] = 0;
+    }
+}
+void Flash::Set(int money_from, int gameCharge_from){
+    money = money_from;
+    gameCharge = gameCharge_from;
+}
+void Flash::GenerateCard(){
+    srand(time(NULL));
+    for(int i=0; i<144; ++i){
+        card[i].a=i;
+        card[i].b=i;
+    }
+    Shuffle(144,card);
+    for(int i=0; i<144; ++i){
+        card[i].a=1+(card[i].a%12);
+        card[i].b=1+(card[i].b/12);
+        if(card[i].a<10){
+            text[i].Add(48+card[i].a);
+            text[i].Add(' ');
+            text[i].Add('x');
+            text[i].Add(' ');
+        }
+        else{
+            text[i].Add(48+card[i].a/10);
+            text[i].Add(48+card[i].a%10);
+            text[i].Add(' ');
+            text[i].Add('x');
+            text[i].Add(' ');
+        }
+        if(card[i].b<10){
+            text[i].Add(48+card[i].b);
+            text[i].Add(' ');
+            text[i].Add('=');
+            text[i].Add(' ');
+        }
+        else{
+            text[i].Add(48+card[i].b/10);
+            text[i].Add(48+card[i].b%10);
+            text[i].Add(' ');
+            text[i].Add('=');
+            text[i].Add(' ');
+        }
+    }
 }
 
+void Flash::Display(){
+    int ans;
+    int terminate = 1;
+    int j = 0, state = 0;
+    char temp[10];
+    char money_dis[100], gc_dis[100];
+    
+    FsOpenWindow(16,16,800,600,1);
+    TextInput t;
+    
+    while(terminate == 1){
+        glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
+        strcpy( money_dis, "Your Money: ");
+        strcpy( gc_dis, "Your Game Charge: ");
+        
+        ans=card[j].a*card[j].b; // record answer
+        t.SetCaption(text[j].GetPointer());
+        t.SetDefaultText("");
+        terminate = t.FlashInput(); // input test: if FSKEY_END, terminate becomes 0
+        if(0!=terminate){
+            if(ans==atoi(t.GetString())){
+                state = 1; // "state" for prolong fssleep
+                glColor3ub(255,255,153);
+                glRasterPos2d(150,300);
+                YsGlDrawFontBitmap20x32("Correct!");
+                money ++;
+                gameCharge ++;
+                sprintf(temp, "%d", money);
+                strncat(money_dis, temp, 12);
+                sprintf(temp, "%d", gameCharge);
+                strncat(gc_dis, temp, 18);
+                glColor3ub(230,230,230);
+                glRasterPos2d(150,350);
+                YsGlDrawFontBitmap20x32(money_dis);
+                glColor3ub(230,230,230);
+                glRasterPos2d(150,400);
+                YsGlDrawFontBitmap20x32(gc_dis);
+            }
+            else{
+                state = 1; // "state" for prolong fssleep
+                glColor3ub(255,153,153);
+                glRasterPos2d(150,300);
+                YsGlDrawFontBitmap20x32("Wrong!");
+                sprintf(temp, "%d", money);
+                strncat(money_dis, temp, 12);
+                sprintf(temp, "%d", gameCharge);
+                strncat(gc_dis, temp, 18);
+                glColor3ub(230,230,230);
+                glRasterPos2d(150,350);
+                YsGlDrawFontBitmap20x32(money_dis);
+                glColor3ub(230,230,230);
+                glRasterPos2d(150,400);
+                YsGlDrawFontBitmap20x32(gc_dis);
+            }
+        }
+        j++;
+        FsSwapBuffers();
+        if (state == 1){ // prolong fssleep
+            FsSleep(1200);
+            state = 0;
+        }
+        FsSleep(0);
+    }
+}
 
+void Flash::Add(){
+    money ++;
+    gameCharge ++;
+}
 
--- Intro.hpp
+++ Intro.hpp
@@ -13,49 +13,7 @@ public:
     TextString GetUser();
 };
 
-template <int number>
-UserIntro<number>::UserIntro(){
-}
-template <int number>
-UserIntro<number>::~UserIntro(){
-    user.CleanUp();
-}
-template <int number>
-void UserIntro<number>::Set(const char from[]){
-    user.Set(from);
-}
-template <int number>
-void UserIntro<number>::Run(){
-    int terminate = 1;
-    
-    //FsOpenWindow(16,16,800,600,1);
-    TextInput t;
-    
-    while(terminate == 1){
-        glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
-        TextString output;
-        char num[5];
-        sprintf(num, "%d", number);
-        output.Set("Please enter user");
-        output.Add(num);
-        output.Add(" name: ");
-        
-        t.SetCaption(output.GetPointer());
-        t.SetDefaultText("");
-        terminate = t.IntroInput(); // input test: if FSKEY_END, terminate becomes 0
-        if(0!=terminate){
-            // printf("%s\n", t.GetString());
-            user.Set(t.GetString());
-            terminate = 0;
-        }
-        FsSwapBuffers();
-        FsSleep(0);
-    }
-}
-template<int number>
-TextString UserIntro<number>::GetUser(){
-    return user;
-}
+// ======================
 
 class UserIntro1: public UserIntro<1>{
 };
@@ -63,14 +21,5 @@ class UserIntro2: public UserIntro<2>{
 };
 
 // ======================
-// main function for Intro
-void Intro(TextString * UserName1, TextString * UserName2){
-    FsOpenWindow(16,16,800,600,1);
-    UserIntro1 user1;
-    UserIntro2 user2;
-    user1.Run();
-    user2.Run();
-    
-    UserName1->Set(user1.GetUser().GetPointer());
-    UserName2->Set(user2.GetUser().GetPointer());
-}
+
+void Intro(TextString * UserName1, TextString * UserName2);
