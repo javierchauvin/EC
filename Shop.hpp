5f684ce1aa47671a0fc519aeabf633dff080da13
Derek Liu <htliu1992@gmail.com>
2015-11-20 10:31:31 -0500

    Derek
    Updata Shop class

--- Shop.hpp
+++ Shop.hpp
@@ -7,7 +7,7 @@
 #include "yspng.h"
 #include "ysglfontdata.h"
 #include <string.h>
-// #include "player.h"
+#include "player.h"
 
 class WeaponText{
 public:
@@ -22,16 +22,18 @@ public:
 class Shop{
 public:
     WeaponText w1, w2, w3, w4, w5, w6, exit;
-    char HaveWeaponStr[50], BuyWeaponStr[50];
+    char HaveWeaponStr[50], BuyWeaponStr[50], NoMoneyStr[50];
     int w1Check, w2Check, w3Check, w4Check, w5Check, w6Check;
     
     Shop();
-    void AssignWeaponCheck(); // add "Player player" as input
-    void AssignToPlayer(); // add "Player player" as input
-    void Run(int &money);
+    void AssignWeaponCheck(Player player); // add "Player player" as input
+    void AssignToPlayer(Player &player); // add "Player player" as input
+    void Run(Player &player, int &status);
     void DrawHaveWeaponStr();
     void DrawBuyWeaponStr();
+    void DrawNoMoneyStr();
     void DrawMoney(int money);
+    void DrawWeaponList();
 };
 
 int  DrawShopWeapon(WeaponText w);
--- TestShop.cpp
+++ TestShop.cpp
@@ -1,12 +1,14 @@
 #include "Shop.hpp"
 
 int main(){
-    int money = 100;
+    int status = 0;
+    Player player1;
+    player1.setMoney(100);
     Shop shop;
-    shop.AssignWeaponCheck(); // Assign weapon list from player to shop
+    shop.AssignWeaponCheck(player1); // Assign weapon list from player to shop
     
     FsOpenWindow(16, 16, 800, 600, 1);
-    shop.Run(money); // should input money
+    shop.Run(player1, status); // should input money
     
     return 0;
 }
