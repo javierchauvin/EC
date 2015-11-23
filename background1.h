13851690bc03b7475f777ca3410c4ee5074b956e
cwang1291 <cwang1291@gmail.com>
2015-11-23 10:13:51 -0500

    background

---
+++ background1.h
@@ -0,0 +1,61 @@
+//
+//  Header.h
+//  framework
+//
+//  Created by Chris Wang on 11/20/15.
+//  Copyright © 2015 24780. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "fssimplewindow.h"
+#include "ysglfontdata.h"
+#include "yspng.h"
+
+#ifndef Header_h
+#define Header_h
+
+class MountainCube
+{
+protected:
+    double x,y;
+    
+public:
+    int state;
+    MountainCube();
+    double GetX();
+    double GetY();
+    void draw();
+    void MakeFromString(char str[]);
+};
+
+class background
+{
+protected:
+    double boundaryX1=200;
+    double boundaryX2=600;
+public:
+    int nCube;
+    int w,h;
+    double GetBoundaryX1();
+    double GetBoundaryX2();
+    MountainCube* CubePtr;
+    void cleanup();
+    void DrawMountain(void);
+    void Initial(void);
+    void Run(weapon* gun);
+    bool checkcollision();
+    void DrawBackground(void);
+    void ReadBackground(void);
+    void MakeFromString(char str[]);
+    void ReadFile();
+    void checkcollision(weapon* gun);
+    YsRawPngDecoder decoder;
+    background();
+    ~background();
+};
+
+
+#endif /* Header_h */
