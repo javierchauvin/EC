14ce3c346c4db64a7e6b7bd4915aaaec6720b85d
wwwrrry <Hsin@HsindeMBP.wv.cc.cmu.edu>
2015-11-30 10:51:51 -0500

    new game and obstacles

--- Game.cpp
+++ Game.cpp
@@ -3,8 +3,8 @@
 void Game::Initial(void)
 {
     background.Initial();
-    weapon.Initial(DEFAULT);//weapon type
-    player.Initial();//x,y,direction
+    weapon1.Initial(DEFAULT);//weapon type
+    player1.Initial();//x,y,direction
     obstacle1.Initial(200, 30, 150, 1, 1);//x,y,size,direction,speed
     obstacle2.Initial(100, 150, 100, 0, 2);//x,y,size,direction,speed
 }
@@ -20,11 +20,13 @@ void Game::Run(int &status)
     {
         terminate=1;
     }
-    background.Run(weapon);
-    weapon.Run(key, player.getX()+80 , player.getY()-37);
-    player.Run();
-    obstacle1.Run(weapon,player);
-    obstacle2.Run(weapon,player);
+    background.Run(weapon1);
+    weapon1.Run(key, player1.getX()+80 , player1.getY()-37);
+    player1.Run();
+    weapon2.Run(key, player2.getX()+80 , player2.getY()-37);
+    player2.Run();
+    obstacle1.Run(weapon1,player1,weapon2,player2);
+    obstacle2.Run(weapon1,player1,weapon2,player2);
     FsSwapBuffers();
     FsSleep(10);
     }
